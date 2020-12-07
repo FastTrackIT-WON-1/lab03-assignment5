0ef075923b839365df76528a794296b1175cc2cf
Florin Lazar <florin.lazar.it2014@gmail.com>
2020-12-07 19:31:27 +0200

    Resolve assignment 5

---
+++ Assignment5/Assignment5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Assignment5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = ReadString("Enter value (attempt 1)=")
+                            ?? ReadString("Enter value (attempt 2)=")
+                            ?? ReadString("Enter value (attempt 3)=")
+                            ?? "nu am continut";
+
+            Console.WriteLine(value);
+        }
+
+        private static string ReadString(string label)
+        {
+            Console.Write(label);
+            string value = Console.ReadLine();
+
+            return string.IsNullOrWhiteSpace(value) ? null : value;
+        }
+    }
+}
